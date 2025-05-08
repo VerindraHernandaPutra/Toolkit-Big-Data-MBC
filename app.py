827295cded57c46650e23f617eb8972415c141aa
Verindra Hernanda Putra <53223736+VerindraHernandaPutra@users.noreply.github.com>
2025-05-08 17:52:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,14 @@ a {color: blue; text-decoration: underline;}
 </style>
 """
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 VALID_USERS = {
